911a078502157032aac0950e13e9b15f48499199
alexander <sadikov.af@ssmu.ru>
2025-08-19 15:25:05 +0700

    add main

---
+++ main.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi import FastAPI, Depends
+
+from app.api.endpoints.transfers import transfer_router
+from app.api.endpoints.user import user_router
+from settings.initial_settings import AppParams
+from settings.logging_settings import configure_logger
+from fake_db.user_db import get_user_manager
+from fake_db.transfer_db import get_transfer_manager
+
+app = FastAPI(dependencies=[Depends(get_user_manager),
+                            Depends(get_transfer_manager),
+                            ],
+              )
+
+app.include_router(transfer_router)
+app.include_router(user_router)
+
+
+if __name__ == "__main__":
+    logger = configure_logger(__name__)
+    logger.info(f"Starting app with {AppParams}")
+
+    uvicorn.run(AppParams.app,
+                host=AppParams.host,
+                port=AppParams.port,
+                reload=AppParams.reload,
+                workers=AppParams.workers,
+                )
