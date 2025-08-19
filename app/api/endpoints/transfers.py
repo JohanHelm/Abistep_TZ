c97800ee36c3e1cc54e900a48ad97e60da8e8a29
alexander <sadikov.af@ssmu.ru>
2025-08-19 15:25:38 +0700

    add endpoints

---
+++ app/api/endpoints/transfers.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.api.models.transfer import TransferCreate, TransferFromDB
+from fake_db.transfer_db import TransferManager, get_transfer_manager
+from fake_db.user_db import get_user_manager, UsersManager
+
+
+transfer_router = APIRouter(
+    prefix="/transfer",
+    tags=["Transfer"],
+)
+
+
+@transfer_router.post("/", response_model=TransferFromDB | dict, status_code=status.HTTP_201_CREATED)
+async def create_transfer(transfer_data: TransferCreate,
+                          transfer_manager: TransferManager = Depends(get_transfer_manager),
+                          user_manager: UsersManager = Depends(get_user_manager),
+                          ):
+
+    if transfer_data.to_user_id == transfer_data.from_user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"don't be naughty")
+
+    sender = user_manager.find_user(int(transfer_data.from_user_id))
+    if sender is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Sender doesn't exist")
+
+    receiver = user_manager.find_user(int(transfer_data.to_user_id))
+    if receiver is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Receiver doesn't exist")
+
+
+    if float(transfer_data.amount) > sender.balance:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"the sender's account does not have "
+                                   f"sufficient funds to complete the transaction")
+
+    new_transfer = transfer_manager.create_transfer(transfer_data)
+
+    return new_transfer
+
+
---
+++ app/api/endpoints/user.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.api.models.user import UserCreate, UserFromDB
+from fake_db.user_db import get_user_manager, UsersManager
+
+
+user_router = APIRouter(
+    prefix="/users",
+    tags=["Users"],
+)
+
+
+@user_router.post("/", response_model=UserFromDB | dict, status_code=status.HTTP_201_CREATED)
+async def create_user(user_data: UserCreate,
+                      user_manager: UsersManager = Depends(get_user_manager),
+                      ):
+
+
+    if user_manager.check_unique_email(user_data):
+        new_user = user_manager.create_new_user(user_data)
+        return new_user
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Users email {user_data.email} is not unique")
+
+
+@user_router.get("/", response_model=list[UserFromDB])
+async def get_users(user_manager: UsersManager = Depends(get_user_manager),
+                    ):
+    return user_manager.users
